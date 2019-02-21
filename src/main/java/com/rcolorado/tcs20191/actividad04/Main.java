af746d216e9d29ac8c72b4bb8c8056399e4a832b
VicMythos <mythosxz9@gmail.com>
2019-02-20 18:16:58 -0600

    REQUERIMIENTO201 Completado

--- src/main/java/com/rcolorado/tcs20191/actividad04/Main.java
+++ src/main/java/com/rcolorado/tcs20191/actividad04/Main.java
@@ -40,7 +40,8 @@ public class Main {
 
         for (int i  = 0; i < lista.size()-2; i++) {
             if (lista.get(i).symbol.equals("localbtc" + divisa.toUpperCase())) {
-                System.out.println(lista.get(i+1).currency + " : " + lista.get(i+1).ask);
+                System.out.println(lista.get(i+1).currency + " : " + lista.get(i+1).ask 
+                        + " venta: " + lista.get(i+1).bid);
             }
         }
         
