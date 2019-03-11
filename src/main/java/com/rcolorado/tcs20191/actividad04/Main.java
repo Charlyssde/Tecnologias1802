f2bc7e8bbc3ef913fbee69b440a8b3335be1ac41
Carlos Carrillo <texcharly9@gmail.com>
2019-03-11 17:58:43 -0600

    Resolución de la observación de severidad más alta de acuerdo a la inspección

--- src/main/java/com/rcolorado/tcs20191/actividad04/Main.java
+++ src/main/java/com/rcolorado/tcs20191/actividad04/Main.java
@@ -19,80 +19,72 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-
-
 /**
  *
  * @author rpimentel
  */
 public class Main {
 
-    public static void main(String[] args) throws IOException  {
+  public static void main(String[] args) throws IOException {
 
-        Scanner scanner = new Scanner(System.in);
-        Date date = new Date();
+    Scanner scanner = new Scanner(System.in);
+    Date date = new Date();
 
-        System.out.println("********************");
-        System.out.println("¿Cuánto vale un Bitcoin en mi divisa?");
-        System.out.println("********************");
-        String divisa;
-        do{
-        System.out.println("Introduce el nombre de tu divisa (MXN, USD, CAD, EUR, ARS, VES):");
+    System.out.println("********************");
+    System.out.println("¿Cuánto vale un Bitcoin en mi divisa?");
+    System.out.println("********************");
+    String divisa;
+    do {
+      System.out.println("Introduce el nombre de tu divisa (MXN, USD, CAD, EUR, ARS, VES):");
 
-        divisa = scanner.next();
-        System.out.println("... espere un momento");
+      divisa = scanner.next();
+      System.out.println("... espere un momento");
 
-        List<DivisaJsonClass> lista = ConsultaBitCoinMarket();
+      List<DivisaJsonClass> lista = ConsultaBitCoinMarket();
 
-       DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-       
-        for (int i  = 0; i < lista.size(); i++) {
-            if (lista.get(i).symbol.equals("localbtc" + divisa.toUpperCase())) {
-                System.out.println(lista.get(i+1).currency + " : " + lista.get(i+1).ask+"La fecha de consulta es: "+dateFormat.format(date));
-            }
+      DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
+      for (int i = 0; i < lista.size(); i++) {
+        if (lista.get(i).symbol.equals("localbtc" + divisa.toUpperCase())) {
+          System.out.println(lista.get(i).currency + " : " + lista.get(i).ask +
+              "\nLa fecha de consulta es: " + dateFormat.format(date));
         }
-        }while(!divisa.toUpperCase().equals("NINGUNA"));
-        
+      }
+    } while (!divisa.toUpperCase().equals("NINGUNA"));
 
-    }
-    
-    
+  }
 
-    
-    
-    private static List<DivisaJsonClass> ConsultaBitCoinMarket() throws IOException {
-        URL url = new URL("http://api.bitcoincharts.com/v1/markets.json");
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        conn.setRequestProperty("Accept", "application/json");
+  private static List<DivisaJsonClass> ConsultaBitCoinMarket() throws IOException {
+    URL url = new URL("http://api.bitcoincharts.com/v1/markets.json");
+    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    conn.setRequestMethod("GET");
+    conn.setRequestProperty("Accept", "application/json");
 
-        if (conn.getResponseCode() != 200) {
-            throw new RuntimeException("Fallo : HTTP error code : " + conn.getResponseCode());
-        }
-        else{
-            System.out.println("No podemos realizar la operacion debido al valor ingresado");
-        }
-
-        InputStreamReader isr = new InputStreamReader(conn.getInputStream());
-        TypeToken<List<DivisaJsonClass>> token = new TypeToken<List<DivisaJsonClass>>() {
-        };
-        List<DivisaJsonClass> lista = new Gson().fromJson(isr, token.getType());
-        
-        return lista;
+    if (conn.getResponseCode() != 200) {
+      throw new RuntimeException("Fallo : HTTP error code : " + conn.getResponseCode());
+    } else {
+      System.out.println("No podemos realizar la operacion debido al valor ingresado");
     }
 
-            
-    /*
+    InputStreamReader isr = new InputStreamReader(conn.getInputStream());
+    TypeToken<List<DivisaJsonClass>> token = new TypeToken<List<DivisaJsonClass>>() {
+    };
+    List<DivisaJsonClass> lista = new Gson().fromJson(isr, token.getType());
+
+    return lista;
+  }
+
+  /*
      * Clase privada para obtener los datos de las divisas
      *
-     */
-            
-    class DivisaJsonClass {
-        // Precio a la venta
-        public double bid;
-        public String currency;
-        // Precio a la compra
-        public double ask;
-        public String symbol;
-    }
+   */
+  class DivisaJsonClass {
+    // Precio a la venta
+
+    public double bid;
+    public String currency;
+    // Precio a la compra
+    public double ask;
+    public String symbol;
+  }
 }
